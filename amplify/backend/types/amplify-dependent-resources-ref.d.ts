059badc53b282c08b1cdce8588e737f9e8f4b2d7
puakehaulani <puakehaulani@gmail.com>
2022-01-25 22:34:28 -0800

    added amplify auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "simpletemplate580e5af2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+// import awsconfig from './aws-exports';
 import Button from 'react-bootstrap/Button'
-
 import './style.css'
+
+// Amplify.configure(awsconfig);
 
 export default function Dashboard() {
 
@@ -9,12 +13,39 @@ export default function Dashboard() {
         alert('Login')
     }
 
+    const [user, setUser] = useState(null);
+    const [customState, setCustomState] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+            switch (event) {
+                case "signIn":
+                    setUser(data);
+                    break;
+                case "signOut":
+                    setUser(null);
+                    break;
+                case "customOAuthState":
+                    setCustomState(data);
+            }
+        });
+
+        Auth.currentAuthenticatedUser()
+            .then(currentUser => setUser(currentUser))
+            .catch(() => console.log("Not signed in"));
+        console.log("user is :", user)
+        return unsubscribe;
+    }, []);
+
+
     return (
         <div className="pageDash pageView">
             <h1 className="text-center">Dashboard</h1>
 
             <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-                <Button variant="dark" onClick={handleLogin}>Login</Button>
+                <Button variant="dark" onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>Sign In</Button>
+                <Button variant="outline-dark" onClick={() => Auth.signOut()}>Sign Out</Button>
+
             </div>
 
         </div>
