1c8cdb2c85872d0cff5d38c827f93b55b8e7e5a2
puakehaulani <puakehaulani@gmail.com>
2022-01-30 20:05:35 -0800

    removed cognito

--- src/index.js
+++ src/index.js
@@ -4,35 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
+// import Amplify from 'aws-amplify';
+// import config from './aws-exports';
 
-// check if env is localhost or not
-// if you're not developing on localhost, you will need to detect this is another way—the docs linked above give some examples. 
-const isLocalhost = !!(window.location.hostname === "localhost");
+// // check if env is localhost or not
+// // if you're not developing on localhost, you will need to detect this is another way—the docs linked above give some examples. 
+// const isLocalhost = !!(window.location.hostname === "localhost");
 
-// split redirect signin and signout strings into correct URIs
-const [
-  productionRedirectSignIn,
-  localRedirectSignIn] = config.oauth.redirectSignIn.split(",");
-const [
-  productionRedirectSignOut,
-  localRedirectSignOut] = config.oauth.redirectSignOut.split(",");
+// // split redirect signin and signout strings into correct URIs
+// const [
+//   productionRedirectSignIn,
+//   localRedirectSignIn] = config.oauth.redirectSignIn.split(",");
+// const [
+//   productionRedirectSignOut,
+//   localRedirectSignOut] = config.oauth.redirectSignOut.split(",");
 
-// use correct URI in the right env
-const updatedAwsConfig = {
-  ...config,
-  oauth: {
-    ...config.oauth,
-    redirectSignIn: isLocalhost
-      ? localRedirectSignIn
-      : productionRedirectSignIn,
-    redirectSignOut: isLocalhost
-      ? localRedirectSignOut
-      : productionRedirectSignOut,
-  }
-}
-Amplify.configure(updatedAwsConfig);
+// // use correct URI in the right env
+// const updatedAwsConfig = {
+//   ...config,
+//   oauth: {
+//     ...config.oauth,
+//     redirectSignIn: isLocalhost
+//       ? localRedirectSignIn
+//       : productionRedirectSignIn,
+//     redirectSignOut: isLocalhost
+//       ? localRedirectSignOut
+//       : productionRedirectSignOut,
+//   }
+// }
+// Amplify.configure(updatedAwsConfig);
 
 ReactDOM.render(
   <React.StrictMode>
