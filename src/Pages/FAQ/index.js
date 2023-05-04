a5a63e598dc8edbc832367971b7b9a28c8d5967e
puakehaulani <puakehaulani@gmail.com>
2023-05-04 11:38:09 -0500

    content updates

--- src/Pages/FAQ/index.js
+++ src/Pages/FAQ/index.js
@@ -9,61 +9,65 @@ export const FAQ = () => {
     const faqList = [
         {
             question: "I'm new, can I come to practice?",
-            answer: ['Reach out to our coaches on our contact form to join an upcoming practice.'],
+            answer: [{ 'description': 'Reach out to our coaches on our contact form to join an upcoming practice', 'link': '/#contact', 'linktext': 'Click here' }],
+
         },
         {
             question: 'How do I pay dues?',
-            answer: ['2023 Dues comprised of two payments. One to KWAC and one to HWOPS Canoe Club.', 'HWOPS club dues electronically via Venmo - Fee structure on Membership form:', 'https://account.venmo.com/u/HuiWaa-PugetSound', 'KWAC Outrigger Membership Fee electronically via paypal - $20:', 'https://www.kenmorewac.org/kwac-membership/'],
+            answer: [{ 'description': '2023 Dues comprised of two payments: one to KWAC and one to HWOPS Canoe Club' }, { 'description': 'See fee structure on Membership form for payment amounts' }, { 'description': 'HWOPS club dues electronically via Venmo', 'link': 'https://account.venmo.com/u/HuiWaa-PugetSound', 'linktext': 'Click here' }, { 'description': 'KWAC Outrigger Membership Fee electronically via paypal', 'link': 'https://www.kenmorewac.org/kwac-membership/', 'linktext': 'Click here' }]
+
         },
         {
             question: 'Where can I obtain the membership form?',
-            answer: ['Membership form is available on Team Cowboy'],
-            link: 'https://www.teamcowboy.com/.../2023%2520Club.../23138'
+            answer: [{ 'description': 'Membership form on TeamCowboy', 'link': 'https://www.teamcowboy.com/.../2023%2520Club.../23138', 'linktext': 'Click here' },],
         },
         {
             question: 'Where do I send the completed registration form?',
-            answer: ['Email forms to HWOPS.contact@gmail.com'],
+            answer: [{ 'description': 'Email forms to ', 'link': 'mailto:HWOPS.contact@gmail.com', 'linktext': 'HWOPS.contact@gmail.com' }],
 
         },
         {
             question: 'How do I know when the next practice is?',
-            answer: ['As of May 1, Paddle Schedule is as follows:', 'Standard Practices start:', 'Tues/Thurs 5:45 PM', 'Sun 9 AM', 'Keiki Practice:', 'Mon/Wed 4:30-6PM', 'Use online team scheduler Team Hero App to reserve your seat:'],
-            link: 'https://hwopscanoeclub.teamcowboy.com/'
+            answer: [{ 'description': 'As of May 1, Paddle Schedule is as follows:' }, { 'description': 'Standard Practices start:' }, { 'description': 'Tues/Thurs 5:45 PM' }, { 'description': 'Sun 9 AM' }, { 'description': 'Keiki Practice:' }, { 'description': 'Mon/Wed 4:30-6PM' }, { 'description': 'Reserve your seat with our online team scheduler', 'link': 'https://hwopscanoeclub.teamcowboy.com/', 'linktext': 'Team Hero App' }],
         },
         {
             question: 'I am bringing a visitor or its my first day, what form do I need to fill out before practice?',
-            answer: ['Online Release if Liability Waiver:'],
-            link: 'https://form.jotform.com/230735968762166'
+            answer: [{ 'link': 'https://form.jotform.com/230735968762166', 'linktext': 'Online Release of Liability Waiver' }],
         },
         {
             question: 'Where does HWOPS practice?',
-            answer: ['7515 NE 175th St, Kenmore, WA 98028'],
-            link: 'https://www.kenmorewa.gov/.../FacilityDirectory/12/103'
+            answer: [{ 'description': "ƛ̕ax̌ʷadis (Tl' awh- ah - dees) Park", 'link': 'https://goo.gl/maps/oN77V5pSbjxrpd3x5', 'linktext': '7515 NE 175th St, Kenmore, WA 98028' }],
         },
         {
             question: 'Is HWOPS hosting a Regatta this year?',
-            answer: ['YES! On June 10, 2023 on Silver Lake, Everett'],
-            link: 'https://pnworca.org/events/silverlake-sprints/'
+            answer: [{ 'description': 'YES! On June 10, 2023 on Silver Lake, Everett' }, { 'linktext': 'Click for more info', 'link': 'https://pnworca.org/events/silverlake-sprints/' }, { 'linktext': 'Silver Lake location', 'link': 'https://goo.gl/maps/VUyyA78CtHPXgj439' }],
         },
         {
             question: 'Information about HWOPS via Kenmore Water Activities Center.',
-            answer: [''],
-            link: 'https://www.kenmorewac.org/outrigger/'
+            answer: [{ 'linktext': 'Click here', 'link': 'https://www.kenmorewac.org/outrigger/' }],
         },
         {
-            question: 'Information about canoe clubs via Pacific Northwest Outrigger Racing Canoe Association.',
-            answer: [''],
-            link: 'https://pnworca.org/directory/'
+            question: 'Information about Canoe Clubs via Pacific Northwest Outrigger Racing Canoe Association.',
+            answer: [{ 'linktext': 'Click here', 'link': 'https://pnworca.org/directory/' }],
         },
 
     ]
     const RenderFAQs = (props) => {
         const renderItem = (i) => {
-            const answers = i.answer.map((i) => {
+            const formattedAnswers = i?.answer?.map((i) => {
+                const formattedLink = (i) => {
+                    return (
+                        <a href={i.link} target='_blank'>
+                            {i.linktext}
+                        </a>
+                    )
+                }
                 return <Row>
-                    {i}
+                    {i.description && i.description}
+                    {i.link && formattedLink(i)}
                 </Row>
             })
+
 
             return (
 
@@ -71,10 +75,7 @@ export const FAQ = () => {
                     <Accordion.Header>{i.question}</Accordion.Header>
                     <Accordion.Body>
                         <Row>
-                            {answers}
-                        </Row>
-                        <Row>
-                            {i.link && <a href={i.link}>Click here</a>}
+                            {formattedAnswers}
                         </Row>
                     </Accordion.Body>
                 </Accordion.Item >
